8b5998eca6c9bbebbd23f57e81e76048fcf01bed
hexagon431 <hexagon431@gmail.com>
2017-10-30 21:02:06 -0600

    Added bootstrap and jquery folders, created skeleton for to-do lists

---
+++ scripts/app.js
@@ -0,0 +1,13 @@
+//The overall list of lists
+let masterList = [];
+
+//List object
+let taskList = function(){
+    this.listName="";
+    this.tasks=[{
+        taskName: "",
+        done: false
+    }]
+
+};
+
