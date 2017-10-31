dd0abaff6fa13ef1c0613472552e18b98f7234c7
hexagon431 <hexagon431@gmail.com>
2017-10-30 21:14:58 -0600

    Tested skeleton with an example test list

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,19 @@
 //The overall list of lists
 let masterList = [];
 
-//List object
-let taskList = function(){
-    this.listName="";
-    this.tasks=[{
-        taskName: "",
-        done: false
-    }]
+//A counter that keeps track of how many lists the user has created.
+let listCount = 0;
 
+//List object - each list has a name and an array of tasks, which are objects. Each task has a name and also a complete/incomplete status.
+//The initial taskList will have an example list for the user.
+let taskList = function(){
+    this.listName="test";
+    this.tasks=[{taskName: "make a list", done: false}]
 };
 
+let testList = new taskList();
+listCount++;
+
+masterList.push(testList);
+
+console.log(masterList[0]);
